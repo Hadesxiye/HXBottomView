a4b928dac3884da82b6bdddc6eb2174b656354ad
王浩祯 <wanghz@changmeng.com>
2019-12-31 21:01:22 +0800

    init
    init

---
+++ HZBottomView/HZBottomView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HZBottomView
+//
+//  Created by 王浩祯 on 2019/12/31.
+//  Copyright © 2019 Hades. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
